7436ff9d15879f9c877e39ad11e1ebd8773e1967
Shane Laymance <shane.laymance@gmail.com>
2020-12-01 14:46:36 -0800

    Set up script to push code to screeps server

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,26 @@
 import babel from '@rollup/plugin-babel';
+import screeps from 'rollup-plugin-screeps';
+import { config } from 'dotenv';
+
+config({ path: `.env.${process.env.DEST}` });
+
+const branchConfig = {
+  token: process.env.TOKEN,
+  protocol: 'https',
+  hostname: 'screeps.com',
+  port: 443,
+  path: '/',
+  branch: 'default'
+};
 
 export default {
   input: 'src/main.js',
   output: {
     file: 'dist/main.js',
-    format: 'esm',
+    format: 'cjs',
   },
-  plugins: [babel({ babelHelpers: 'bundled' })],
+  plugins: [
+    babel({ babelHelpers: 'bundled' }),
+    screeps({ config: branchConfig, dryRun: null })
+  ],
 };
